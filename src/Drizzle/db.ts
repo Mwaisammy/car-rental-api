a903ccb24c5f0102a1e241608d091525f9f4d2ef
Mwaisammy <mwaisamuel432@gmail.com>
2025-07-01 15:14:23 +0300

    connected neon db

--- src/Drizzle/db.ts
+++ src/Drizzle/db.ts
@@ -1,24 +1,39 @@
 /*istanbul ignore next*/
-import "dotenv/config";
+// import "dotenv/config";
 
-import { drizzle } from "drizzle-orm/node-postgres"
-// import { drizzle } from "drizzle-orm/neon-http"
+// import { drizzle } from "drizzle-orm/node-postgres"
+// // import { drizzle } from "drizzle-orm/neon-http"
 
-import { Client } from 'pg'
-import * as schema from "./schema"
-// import { neon } from "@neondatabase/serverless";
+// import { Client } from 'pg'
+// import * as schema from "./schema"
+// // import { neon } from "@neondatabase/serverless";
 
-export const client = new Client({
-    connectionString: process.env.Database_URL as string,
-})
+// export const client = new Client({
+//     connectionString: process.env.Database_URL as string,
+// })
 
 // export const client = neon(process.env.Database_URL!)
 
-const main  = async() => {
-    await client.connect();
+// const main  = async() => {
+//     await client.connect();
+// }
+
+// main()
+
+// const db = drizzle(client, { schema, logger:false});
+// export default db;
+
+
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import "dotenv/config";
+import * as schema from "./schema";
+
+export const client = neon(process.env.Database_URL!)
+if(!client){
+    throw new Error("Database connection failed")
 }
 
-main()
-
-const db = drizzle(client, { schema, logger:false});
+const db = drizzle(client, { schema, logger: false })
 export default db;
+
